3f02c29ab95d7b3d6eb041c759a821bf58c34098
yasir227 <yasir.turzo@gmail.com>
2024-06-14 11:07:25 -0700

    first commit

---
+++ Desktop/Data_structure & ALGO/static_array.py
@@ -0,0 +1,7 @@
+### assist 1
+# create a list with a fixed size of 5, initialized with ones
+numbers = [1] * 5
+# printing all values of the list
+print("Assist 1")
+for number in numbers:
+    print(number)
